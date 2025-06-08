88636562fdbcd37e498e98f6c4ac674173484b94
JackMulcunry <165406093+JackMulcunry@users.noreply.github.com>
2025-06-08 13:45:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { tempo } from 'tempo-devtools/dist/vite';
+import { tempo } from "tempo-devtools/dist/vite";
 
 export default defineConfig({
-  base: "/running-blog/", // ✅ Required for GitHub Pages
+  base: "/running-blog/",
   plugins: [react(), tempo()],
+  define: {
+    'import.meta.env.VITE_TEMPO': JSON.stringify(true), // <-- force it always true for now
+  },
 });
