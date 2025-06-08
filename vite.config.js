cb8ac92951d7595c6445d68379b54381b78e2bde
JackMulcunry <165406093+JackMulcunry@users.noreply.github.com>
2025-06-08 14:17:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tempo } from "tempo-devtools/dist/vite";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/running-blog/" : "/", // ✅ correct base per env
-  plugins: [react(), tempo()],
-}));
+export default defineConfig({
+  base: '/running-blog/',
+  plugins: [react()],
+})
