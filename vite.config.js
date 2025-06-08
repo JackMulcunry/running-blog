a425ec851a1f615e635fcbb29a0d5ad6107ba6a1
JackMulcunry <165406093+JackMulcunry@users.noreply.github.com>
2025-06-08 12:08:37 -0400

    Update vite.config.js
    update path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 export default defineConfig({
   plugins: [react(), tempo()],
+  base: "/running-blog/", // ✅ <-- Add this line
   server: {
     // @ts-ignore
     allowedHosts: process.env.TEMPO === "true" ? true : undefined,
