df2fe2e112a6c4b796e53f06880ccfb99a02677a
JackMulcunry <jack.mulcunry@gmail.com>
2025-06-08 11:48:48 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My Running Blog</h1>
+      <p>Tracking daily performance and insights.</p>
+      <div id="blog-posts">
+        {/* NEW POSTS GET INSERTED HERE BY N8N */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
